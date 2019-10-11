9dbdc1e837467cbfdd0cee7dc3b6849ab1300ae4
zxmapleliu <610307812@qq.com>
2019-10-11 15:47:17 +0800

    添加项目文件。

---
+++ OpenGLCourse1/main.cpp
@@ -0,0 +1,159 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+using namespace std;
+
+//使用GLSL定义顶点着色器
+const GLchar* vertexShaderSource =
+"#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position.x,position.y,position.z,1.0);\n"
+"}\0";
+//使用GLSL定义片段着色器
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f,0.5f,0.2f,1.0f);\n"
+"}\n\0";
+
+//窗口调整大小的回调函数
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main(int argc,char**argv)
+{
+	//实例化GLFW窗口
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//创建一个窗口对象
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "OpenGLCourse1", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Failed to create GLFW window" << endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//注册回调函数
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	//初始化GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+//	//编译顶点着色器
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//编译片段着色器
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//创建着色器程序
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	//连接着色器
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	//删除不用的着色器
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//输入顶点数据
+	GLfloat vertices[] = {
+		-0.5f,-0.5f,0.0f,
+		0.5f,-0.5f,0.0f,
+		0.5f,0.5f,0.0f,
+		0.5f,0.5f,0.5f
+	};
+
+	unsigned int indices[] =
+	{
+		0,1,3,
+		1,2,3,
+		0,1,2,
+		0,2,3
+	};
+
+	//创建顶点缓冲对象VBO,VAO，EBO
+	unsigned int VBO;
+	unsigned int VAO;
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	//将数组与VBO绑定
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//将EBO绑定
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//告诉VAO怎么解析
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
+	//不断绘制，接受用户输入
+	while (!glfwWindowShouldClose(window))
+	{
+		//接受输入
+		processInput(window);
+		//渲染
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//画图形
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	//退出前删除VAO VBO 释放显存
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glfwTerminate();
+	return 0;
+}
