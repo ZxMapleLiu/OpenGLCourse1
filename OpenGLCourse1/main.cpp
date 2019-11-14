c48454e7bdfa34d7926b20675084653bde85d80b
zxmapleliu <610307812@qq.com>
2019-11-14 20:50:22 +0800

    增加摄像机

--- OpenGLCourse1/main.cpp
+++ OpenGLCourse1/main.cpp
@@ -8,47 +8,30 @@
 #include<glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
+#include "camera.h"
 #include "stb_image.h"
-/*using namespace std;*/
-// 
-// //使用GLSL定义顶点着色器
-// const GLchar* vertexShaderSource =
-// "#version 330 core\n"
-// "layout (location = 0) in vec3 position;\n"//位置变量属性位置值为0
-// "layout (location = 1) in vec3 aColor;\n"//颜色变量属性位置值为1
-// "out vec3 vertexColor;\n"
-// "void main()\n"
-// "{\n"
-// "gl_Position = vec4(position,1.0);\n"
-// //"vertexColor = vec4(0.5f,0.0f,0.0f,1.0f);\n"
-// "vertexColor = aColor;\n"
-// "}\0";
-// //使用GLSL定义片段着色器
-// const GLchar* fragmentShaderSource = "#version 330 core\n"
-// "out vec4 color;\n"
-// //"uniform vec4 ourColor;\n"
-// "in vec3 vertexColor;\n"
-// "void main()\n"
-// "{\n"
-// //"color = ourColor;\n"// 使用uniform传来的颜色信息
-// "color = vec4(vertexColor,1.0);\n" //使用顶点着色器传来的颜色信息
-// "}\n\0";
 
-const unsigned int SCR_WIDTH = 1024;
-const unsigned int SCR_HEIGHT = 768;
-
-
+const unsigned int SCR_WIDTH = 1600;
+const unsigned int SCR_HEIGHT = 900;
 //窗口调整大小的回调函数
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+
+
+
 
 int main(int argc, char** argv)
 {
@@ -57,6 +40,7 @@ int main(int argc, char** argv)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
 
 	//创建一个窗口对象
 	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLCourse1", NULL, NULL);
@@ -66,61 +50,18 @@ int main(int argc, char** argv)
 		glfwTerminate();
 		return -1;
 	}
+	//设置鼠标隐藏光标
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwMakeContextCurrent(window);
 	//注册回调函数
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	//初始化GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-// 	//	//编译顶点着色器
-// 	unsigned int vertexShader;
-// 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-// 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-// 	glCompileShader(vertexShader);
-// 	int success;
-// 	char infoLog[512];
-// 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-// 	if (!success)
-// 	{
-// 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//编译片段着色器
-// 	unsigned int fragmentShader;
-// 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-// 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-// 	glCompileShader(fragmentShader);
-// 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-// 	if (!success)
-// 	{
-// 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//创建着色器程序
-// 	unsigned int shaderProgram;
-// 	shaderProgram = glCreateProgram();
-// 	//连接着色器
-// 	glAttachShader(shaderProgram, vertexShader);
-// 	glAttachShader(shaderProgram, fragmentShader);
-// 	glLinkProgram(shaderProgram);
-// 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-// 	if (!success) {
-// 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//删除不用的着色器
-// 	glDeleteShader(vertexShader);
-// 	glDeleteShader(fragmentShader);
-	//输入顶点数据
-// 	GLfloat vertices[] = {
-// 		-0.5f,-0.5f,0.0f,
-// 		0.5f,-0.5f,0.0f,
-// 		0.5f,0.5f,0.0f,
-// 		0.5f,0.5f,0.5f
-// 	};
 	float vertices[] = {
 		//位置 纹理
 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -165,6 +106,19 @@ int main(int argc, char** argv)
 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 	};
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 	//纹理坐标
 	//可以写进顶点坐标数组
 // 	GLfloat texCoords[] = {
@@ -199,7 +153,7 @@ int main(int argc, char** argv)
 	//纹理
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-
+	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	/*	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//描边模式*/
@@ -274,11 +228,19 @@ int main(int argc, char** argv)
 	//将变换矩阵转换到uniform中
 	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
 	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
-
+	//投影矩阵
+	glm::mat4 projection = glm::mat4(1.0f);
+	projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	ourShader.setMat4("projection", projection);
 
 	//不断绘制，接受用户输入
 	while (!glfwWindowShouldClose(window))
 	{
+		//计算deltatime的逻辑
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//接受输入
 		processInput(window);
 		//渲染
@@ -298,32 +260,28 @@ int main(int argc, char** argv)
 // 		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		ourShader.use();
-		//进入3D！模型矩阵将模型转换到世界中来
-		glm::mat4 model = glm::mat4(1.0f); // 记得初始化矩阵
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
-		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		//将模型和摄像机位置转换为uniform并传入shader
-		unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-		unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-		// 传入shader
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-		// 虽然我们在循环内设置，但他没有形变，所以可以放在外面
-		ourShader.setMat4("projection", projection);
+		// camera/view transformation
+		glm::mat4 view = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+		//我们要做的只是定义一个摄像机位置，一个目标位置和一个表示世界空间中的上向量的向量（我们计算右向量使用的那个上向量）。接着GLM就会创建一个LookAt矩阵
+		view = camera.GetViewMatrix();
+		ourShader.setMat4("view", view);
 
-// 		unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-// 		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-		// 		//用uniform更新颜色
-		// 		float timeValue = glfwGetTime();
-		// 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-		// 		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-		// 		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-				//
+		// render boxes
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 		//
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -336,4 +294,57 @@ int main(int argc, char** argv)
 	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
+}
+// 处理输入
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// 改变窗口大小的处理
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// 鼠标移动的回调函数
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// 滚轮的回调函数
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
 }
