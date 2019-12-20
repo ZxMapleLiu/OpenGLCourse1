4b74d91f6eacfa9ba789176994096932a291ccc4
ZxMapleLiu <610307812@QQ.COM>
2019-12-20 12:39:25 +0800

    加入方向控制

--- OpenGLCourse1/main.cpp
+++ OpenGLCourse1/main.cpp
@@ -10,47 +10,79 @@
 #include "shader.h"
 #include "stb_image.h"
 #include "model.h"
-/*using namespace std;*/
-// 
-// //使用GLSL定义顶点着色器
-// const GLchar* vertexShaderSource =
-// "#version 330 core\n"
-// "layout (location = 0) in vec3 position;\n"//位置变量属性位置值为0
-// "layout (location = 1) in vec3 aColor;\n"//颜色变量属性位置值为1
-// "out vec3 vertexColor;\n"
-// "void main()\n"
-// "{\n"
-// "gl_Position = vec4(position,1.0);\n"
-// //"vertexColor = vec4(0.5f,0.0f,0.0f,1.0f);\n"
-// "vertexColor = aColor;\n"
-// "}\0";
-// //使用GLSL定义片段着色器
-// const GLchar* fragmentShaderSource = "#version 330 core\n"
-// "out vec4 color;\n"
-// //"uniform vec4 ourColor;\n"
-// "in vec3 vertexColor;\n"
-// "void main()\n"
-// "{\n"
-// //"color = ourColor;\n"// 使用uniform传来的颜色信息
-// "color = vec4(vertexColor,1.0);\n" //使用顶点着色器传来的颜色信息
-// "}\n\0";
+#include "camera.h"
 
 const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 900;
-
-
 //窗口调整大小的回调函数
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
 
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+// 处理输入
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
+// 改变窗口大小的处理
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
+
+
+// 鼠标移动的回调函数
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// 滚轮的回调函数
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
 int main(int argc, char** argv)
 {
 	//实例化GLFW窗口
@@ -67,244 +99,44 @@ int main(int argc, char** argv)
 		glfwTerminate();
 		return -1;
 	}
+	//设置鼠标隐藏光标
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwMakeContextCurrent(window);
 	//注册回调函数
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 	//初始化GLAD
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
-// 	//	//编译顶点着色器
-// 	unsigned int vertexShader;
-// 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
-// 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-// 	glCompileShader(vertexShader);
-// 	int success;
-// 	char infoLog[512];
-// 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-// 	if (!success)
-// 	{
-// 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//编译片段着色器
-// 	unsigned int fragmentShader;
-// 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-// 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-// 	glCompileShader(fragmentShader);
-// 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-// 	if (!success)
-// 	{
-// 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//创建着色器程序
-// 	unsigned int shaderProgram;
-// 	shaderProgram = glCreateProgram();
-// 	//连接着色器
-// 	glAttachShader(shaderProgram, vertexShader);
-// 	glAttachShader(shaderProgram, fragmentShader);
-// 	glLinkProgram(shaderProgram);
-// 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-// 	if (!success) {
-// 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-// 		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-// 	}
-// 	//删除不用的着色器
-// 	glDeleteShader(vertexShader);
-// 	glDeleteShader(fragmentShader);
-	//输入顶点数据
-// 	GLfloat vertices[] = {
-// 		-0.5f,-0.5f,0.0f,
-// 		0.5f,-0.5f,0.0f,
-// 		0.5f,0.5f,0.0f,
-// 		0.5f,0.5f,0.5f
-// 	};
-// 	float vertices[] = {
-// 		//位置 纹理
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-// 	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
-// 	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-// 	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-// 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
-// 
-// 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-// 	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-// 	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
-// 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-// 
-// 	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-// 	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-// 	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
-// 	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-// 	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
-// 	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
-// 	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
-// 
-// 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
-// 	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-// 	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
-// 	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
-// 	};
-	//纹理坐标
-	//可以写进顶点坐标数组
-// 	GLfloat texCoords[] = {
-// 		0.0f,0.0f,
-// 		1.0f,0.0f,
-// 		0.5f,1.0f
-// 	};
-// 	unsigned int indices[] =
-// 	{
-// 		0,1,3,//第一个三角形
-// 		1,2,3,//第二个三角形
-// 	};
-
-	//创建顶点缓冲对象VBO,VAO，EBO
-// 	unsigned int VBO;
-// 	unsigned int VAO;
-// 	unsigned int EBO;
-// 	glGenBuffers(1, &EBO);
-// 	glGenVertexArrays(1, &VAO);
-// 	glGenBuffers(1, &VBO);
-// 	glBindVertexArray(VAO);
-// 	//将数组与VBO绑定
-// 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-// 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-// 	//将EBO绑定
-// 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-// 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-// 	//告诉VAO怎么解析
-// 	//位置
-// 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-// 	glEnableVertexAttribArray(0);
-// 	//纹理
-// 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-// 	glEnableVertexAttribArray(1);
-// 
-// 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-// 	glBindVertexArray(0);
-	/*	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//描边模式*/
-	//深度测试！
-
-// 	//第一个纹理
-// 	unsigned int texture1;
-// 	glGenTextures(1, &texture1);
-// 	glBindTexture(GL_TEXTURE_2D, texture1);
-// 	//调整纹理的环绕方式为重复
-// 	//坐标轴：S,R,T
-// 	//GL_REPEAT,GL_MIRRORED_REPEAT,GL_CLAMP_TO_BORDER,GL_CLAMP_TO_EDGE
-// 	//如果使用GL_CLAMP_TO_BORDER, 还需要定义边界颜色值
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-// 	// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//将缩小（Minify）时的纹理过滤选项调整为邻近纹理过滤
-// 	// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//将放大（Magnify）时纹理过滤选项调整为线性纹理过滤
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);//多级渐远纹理
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-// 	//加载纹理
-// 	int width, height, nrChannels;
-// 	unsigned char* data = stbi_load("./texture/container.jpg", &width, &height, &nrChannels, 0);
-// 	if (data)
-// 	{
-// 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-// 		glGenerateMipmap(GL_TEXTURE_2D);
-// 	}
-// 	else
-// 	{
-// 		std::cout << "Failed to load texture" << std::endl;
-// 	}
-// 	stbi_image_free(data);
-// 	stbi_set_flip_vertically_on_load(true);//翻转Y轴
-// 
-// 	//第一个纹理
-// 	unsigned int texture2;
-// 	glGenTextures(1, &texture2);
-// 	glBindTexture(GL_TEXTURE_2D, texture2);
-// 	//调整纹理的环绕方式为重复
-// 	//坐标轴：S,R,T
-// 	//GL_REPEAT,GL_MIRRORED_REPEAT,GL_CLAMP_TO_BORDER,GL_CLAMP_TO_EDGE
-// 	//如果使用GL_CLAMP_TO_BORDER, 还需要定义边界颜色值
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-// 	// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);//将缩小（Minify）时的纹理过滤选项调整为邻近纹理过滤
-// 	// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);//将放大（Magnify）时纹理过滤选项调整为线性纹理过滤
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);//多级渐远纹理
-// 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-// 	//加载纹理
-// 	data = stbi_load("./texture/awesomeface.png", &width, &height, &nrChannels, 0);
-// 	if (data)
-// 	{
-// 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-// 		glGenerateMipmap(GL_TEXTURE_2D);
-// 	}
-// 	else
-// 	{
-// 		std::cout << "Failed to load texture" << std::endl;
-// 	}
-// 	stbi_image_free(data);
-// 	ourShader.use();
-// 	//手动设置uniform的ID
-// 	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
-// 	//或者使用shader的类函数设置
-// 	ourShader.setInt("texture2", 1);
-// 	//形变矩阵
-// 	glm::mat4 trans = glm::mat4(1.0f);
-// 	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-// 	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
-// 	//将变换矩阵转换到uniform中
-// 	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
-// 	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 	glEnable(GL_DEPTH_TEST);
 
 	Shader ourShader("./shaders/shader.vert", "./shaders/shader.frag"); //使用我们自己定义的着色器
 
 	Model ourModel(("./model/AIBoss/model.obj"));
+
 	//不断绘制，接受用户输入
 	while (!glfwWindowShouldClose(window))
 	{
+		//计算deltatime的逻辑
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
 		//接受输入
 		processInput(window);
 		//渲染
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //深度测试！
-		// 		//画图形
-		// 		glUseProgram(shaderProgram);    // 使用自己的着色器时仅有这一行被替换了，
-												//程序中前面创建vertexShader和fragmentShader的部分也可以删除
-// 		glActiveTexture(GL_TEXTURE0);
-// 		glBindTexture(GL_TEXTURE_2D, texture1);
-// 		glActiveTexture(GL_TEXTURE1);
-// 		glBindTexture(GL_TEXTURE_2D, texture2);
-
-// 		// 进行变换
-// 		glm::mat4 transform = glm::mat4(1.0f); // 先初始化矩阵
-// 		transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-// 		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		ourShader.use();
 		//进入3D！模型矩阵将模型转换到世界中来
 		glm::mat4 model = glm::mat4(1.0f); // 记得初始化矩阵
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 projection = glm::mat4(1.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		glm::mat4 view = camera.GetViewMatrix();
 		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		view = glm::translate(view, glm::vec3(0.0f, -5.0f, -25.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
